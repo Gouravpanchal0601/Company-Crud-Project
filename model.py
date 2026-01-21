6867690152d0ea8dcdb66d52f37e8fe44c14a0e5
Gourav panchal <160009834+Gouravpanchal0601@users.noreply.github.com>
2026-01-21 19:34:49 +0530

    Add files via upload

--- model.py
+++ model.py
@@ -1,15 +1,23 @@
-from sqlalchemy import Column, Integer, String, Boolean, ForeignKey  
+from sqlalchemy import Column, Integer, String, Boolean, ForeignKey, DateTime
 from sqlalchemy.orm import relationship
+from datetime import datetime
 from database import Base
 
 class User(Base):
-    __tablename__="users"
-    id = Column(Integer, primary_key=True, index=True)
-    username = Column(String, unique = True, index=True, nullable=False)
-    hashed_password = Column(String, nullable=False)
-    is_active = Column(Boolean, default = True)
+    __tablename__ = "auth_users"
 
-    employees = relationship("Employee",back_populates="owner")
+    id = Column(Integer, primary_key=True, index=True)
+    username = Column(String, unique=True, nullable=False)
+    email = Column(String, unique=True, nullable=False)
+    hashed_password = Column(String, nullable=False)
+    role = Column(String, nullable=False)
+    is_verified = Column(Boolean, default=False)
+    otp_code = Column(String, nullable=True)
+    otp_expiry = Column(DateTime, nullable=True)
+    created_at = Column(DateTime, default=datetime.utcnow)
+
+    employees = relationship("Employee", back_populates="owner")
+
 
 class Employee(Base):
     __tablename__ = "employees"
@@ -18,6 +26,6 @@ class Employee(Base):
     name = Column(String)
     age = Column(Integer)
     department = Column(String)
-    owner_id = Column(Integer, ForeignKey("users.id"),nullable=True)
 
+    owner_id = Column(Integer, ForeignKey("auth_users.id"), nullable=False)
     owner = relationship("User", back_populates="employees")
